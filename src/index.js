2aed4967492f16193fa1b7eec7b77b5560bf5a15
jetfuzz <1jsf23@gmail.com>
2025-07-09 08:48:18 -0500

    Add basic search form and console functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import "./style.css";
 import * as api from "./api";
 
+const searchForm = document.querySelector(".search-form");
+
 async function showWeatherData(location) {
     let weatherData = await api.getWeatherData(location);
     let processedWeatherData = api.processWeatherData(weatherData);
     return processedWeatherData;
 }
 
-console.log(showWeatherData("Vancouver"));
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const searchQuery =  document.getElementById("search-input").value;
+    console.log(showWeatherData(searchQuery));
+})
